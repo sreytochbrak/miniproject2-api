86627bbf56182db3b17be403f65584ff3e20cf3e
Sreytoch-Brak <137994391+sreytochbrak@users.noreply.github.com>
2025-06-08 18:13:15 +0700

    Update api-gateway.js

--- api-gateway.js
+++ api-gateway.js
@@ -14,7 +14,7 @@ app.use('/teacher', createProxyMiddleware({
   changeOrigin: true
 }));
 
-app.listen(3000, () => {
-  console.log("API Gateway is running on port 3000");
+app.listen(5000, () => {
+  console.log("API Gateway is running on port 5000");
 });
 
