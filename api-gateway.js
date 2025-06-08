f78f16268eeaeef9daf992cdbe29aeb0a19ead1d
Sreytoch-Brak <137994391+sreytochbrak@users.noreply.github.com>
2025-06-08 17:07:56 +0700

    Update api-gateway.js

--- api-gateway.js
+++ api-gateway.js
@@ -1,11 +1,20 @@
-
-
-
 const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 const app = express();
 
-app.use('/student', createProxyMiddleware({ target: 'http://localhost:3001', changeOrigin: true }));
-app.use('/teacher', createProxyMiddleware({ target: 'http://localhost:3002', changeOrigin: true }));
+// Forward requests to Student Microservice
+app.use('/student', createProxyMiddleware({
+  target: 'http://3.85.135.131',
+  changeOrigin: true
+}));
 
-app.listen(3000, () => console.log('API Gateway running on 3000'));
+// Forward requests to Teacher Microservice
+app.use('/teacher', createProxyMiddleware({
+  target: 'http://13.219.97.195',
+  changeOrigin: true
+}));
+
+app.listen(3000, () => {
+  console.log("API Gateway is running on port 3000");
+});
+
